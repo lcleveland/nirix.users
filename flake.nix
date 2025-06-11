a279f0130d9183312ccaa9d1999f7025e1fffde1
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 14:39:11 -0500

    remove niri

--- flake.nix
+++ flake.nix
@@ -1,19 +1,15 @@
 {
   description = "Reusable NixOS user configurations";
-  outputs = { self, niri, nixpkgs, nixvim, zen_browser }@inputs:
+  outputs = { self, nixpkgs, nixvim, zen_browser }@inputs:
     let
       import_modules = import ./resources/nix/import_modules.nix;
-      users = import_modules ./users ++ [ niri.nixosModules.niri ];
+      users = import_modules ./users;
     in
     {
       inputs = inputs;
       nixosModules.default = users;
     };
   inputs = {
-    niri = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:sodiboo/niri-flake";
-    };
     nixvim = {
       url = "github:nix-community/nixvim?ref=nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,10 +19,6 @@
     };
   };
 }
-
-
-
-
 
 
 
