d334a334475668b7abb5f4f2260945023441ae4f
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 17:33:11 -0500

    test

--- flake.nix
+++ flake.nix
@@ -1,15 +1,26 @@
 {
   description = "Reusable NixOS user configurations";
-  outputs = { self, nixpkgs, nixvim, zen_browser }@inputs:
+  outputs = { self, home_manager, niri, nixpkgs, nixvim, zen_browser }@inputs:
     let
       import_modules = import ./resources/nix/import_modules.nix;
-      users = import_modules ./users;
+      users = (import_modules ./users) ++
+        [
+          niri.nixosModules.niri
+        ];
     in
     {
       inputs = inputs;
       nixosModules.default = users;
     };
   inputs = {
+    home_manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager?ref=release-25.05";
+    };
+    niri = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:sodiboo/niri-flake";
+    };
     nixvim = {
       url = "github:nix-community/nixvim?ref=nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,6 +30,10 @@
     };
   };
 }
+
+
+
+
 
 
 
