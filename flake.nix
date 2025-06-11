2844d935559397aeb4ab2d0e82319748ed585342
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 20:41:56 -0500

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,1 +1,22 @@
-{ }
+{
+  description = "Reusable NixOS user configurations";
+  outputs = { self, home-manager, nixpkgs, nixvim, zen_browser }@inputs:
+    let
+      import_modules = import ./resources/nix/import_modules.nix;
+      users = import_modules ./users;
+    in
+    {
+      inputs = inputs;
+      nixosModules.default = users;
+    };
+  inputs = {
+    nixvim = {
+      url = "github:nix-community/nixvim?ref=nixos-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen_browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+    };
+  };
+}
+
