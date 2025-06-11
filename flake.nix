732b4afda1a08bfca27864ddf8e8f5d8d9435b55
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 16:53:49 -0500

    fix missing

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Reusable NixOS user configurations";
-  outputs = { self, nixpkgs, nixvim, zen_browser }@inputs:
+  outputs = { self, niri, nixpkgs, nixvim, zen_browser }@inputs:
     let
       import_modules = import ./resources/nix/import_modules.nix;
       users = import_modules ./users;
@@ -23,6 +23,7 @@
     };
   };
 }
+
 
 
 
