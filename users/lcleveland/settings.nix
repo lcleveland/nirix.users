4199cf9b52fbccc4a81e50ddf0964f450a2c448e
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 14:34:08 -0500

    test

--- users/lcleveland/settings.nix
+++ users/lcleveland/settings.nix
@@ -1,11 +1,11 @@
-{ config, home-manager, lib, niri, pkgs, ... }:
+{ config, home-manager, inputs, lib, pkgs, ... }:
 let
   user = config.nirix.users.lcleveland.settings;
 in
 {
   config = lib.mkIf user.enable {
     home-manager.users.lcleveland = {
-      imports = [ niri.homeModules.niri ];
+      imports = [ inputs.niri.homeModules.niri ];
       home = {
         stateVersion = user.state_version;
         username = user.username;
