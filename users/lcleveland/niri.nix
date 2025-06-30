27636a31ff623efda8efb155f0dfe46235bd5baa
Lyle Cleveland <lyle.cleveland@proton.me>
2025-06-30 08:29:25 -0500

    output test

--- users/lcleveland/niri.nix
+++ users/lcleveland/niri.nix
@@ -1,2 +1,10 @@
 { config, lib, ... }:
-{ }
+{
+  home-manager.users.lcleveland.programs.niri.settings = {
+    outputs = {
+      "Lenovo Group Limited LEN T24i-10 VNA4B1P6" = {
+        transform = "flipped";
+      };
+    };
+  };
+}
