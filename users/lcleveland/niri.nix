d6a28931cbda628d9e0564ffdbdb9b500a0d72f9
Lyle Cleveland <lyle.cleveland@proton.me>
2025-06-30 08:43:28 -0500

    output test

--- users/lcleveland/niri.nix
+++ users/lcleveland/niri.nix
@@ -2,8 +2,24 @@
 {
   home-manager.users.lcleveland.programs.niri.settings = {
     outputs = {
+      "eDP-1" = {
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
       "Lenovo Group Limited LEN T24i-10 VNA4B1P6" = {
+        position = {
+          x = -1920;
+          y = 1080;
+        };
         transform.rotation = 180;
+      };
+      "Lenovo Group Limited LEN T24i-10 VNA4B1PL" = {
+        position = {
+          x = -1920;
+          y = 0;
+        };
       };
     };
   };
